46d954d70aa02174bef216e7770ec0f4c13b540f
John Kelly <john.kelly@digital.hmrc.gov.uk>
2020-02-25 09:42:25 +0000

    Refactor package structure

--- test/controllers/MicroserviceHelloWorldControllerSpec.scala
+++ test/controllers/MicroserviceHelloWorldControllerSpec.scala
@@ -1,4 +1,20 @@
-package uk.gov.hmrc.commontransitconventiontraders.controllers
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
 
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
@@ -7,7 +23,7 @@ import play.api.http.Status
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, Helpers}
 import uk.gov.hmrc.play.bootstrap.config.{RunMode, ServicesConfig}
-import uk.gov.hmrc.commontransitconventiontraders.config.AppConfig
+import config.AppConfig
 
 class MicroserviceHelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
 
