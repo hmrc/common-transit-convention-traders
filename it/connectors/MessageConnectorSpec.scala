311e9ebfd104adbb68e31c2e022a0c46fe3b8f2c
John Kelly <john.kelly@digital.hmrc.gov.uk>
2020-03-03 15:32:23 +0000

    Generate test data

--- it/connectors/MessageConnectorSpec.scala
+++ it/connectors/MessageConnectorSpec.scala
@@ -2,17 +2,19 @@ package connectors
 
 import org.scalatest.{FreeSpec, MustMatchers}
 import com.github.tomakehurst.wiremock.client.WireMock._
+import org.scalacheck.Gen
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.HeaderCarrier
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class MessageConnectorSpec extends FreeSpec with MustMatchers with WiremockSuite with ScalaFutures with IntegrationPatience {
-  private val connector = app.injector.instanceOf[MessageConnector]
-
+class MessageConnectorSpec extends FreeSpec with MustMatchers with WiremockSuite with ScalaFutures with IntegrationPatience with ScalaCheckPropertyChecks {
   "post" - {
     "must return NO_CONTENT when post is successful" in {
+      val connector = app.injector.instanceOf[MessageConnector]
+
       server.stubFor(
         post(
           urlEqualTo("/common-transit-convention-trader-at-destination/message-notification")
@@ -24,6 +26,31 @@ class MessageConnectorSpec extends FreeSpec with MustMatchers with WiremockSuite
       val result = connector.post(<document></document>).futureValue
 
       result.status mustEqual NO_CONTENT
+    }
+
+    "throw exception when post is unsuccessful" in {
+      val connector = app.injector.instanceOf[MessageConnector]
+
+      val errorCodes = Gen.choose(400, 599)
+
+      forAll(errorCodes) {
+        errorCode =>
+
+          server.stubFor(
+            post(
+              urlEqualTo("/common-transit-convention-trader-at-destination/message-notification")
+            ).willReturn(aResponse().withStatus(errorCode))
+          )
+
+          implicit val hc = HeaderCarrier()
+
+          val result = connector.post(<document></document>)
+
+          whenReady(result.failed) {
+            response =>
+              response mustBe an[Exception]
+          }
+      }
     }
   }
 
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,11 +12,11 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.4.0" % Test classifier "tests",
-    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
+    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test, it",
+    "com.typesafe.play"       %% "play-test"                % current                 % "test, it",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it",
-    "org.scalacheck"          %% "scalacheck"               % "1.14.1"                % "test",
+    "org.scalacheck"          %% "scalacheck"               % "1.14.1"                % "test, it",
     "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.17.0"                % "it"
   )
 
