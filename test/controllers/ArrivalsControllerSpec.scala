4ed20ac092c405404861726f60b5950506b133f7
John Kelly <john.kelly@digital.hmrc.gov.uk>
2020-03-16 15:45:13 +0000

    Return Location header on successful IE007 POST

--- test/controllers/ArrivalsControllerSpec.scala
+++ test/controllers/ArrivalsControllerSpec.scala
@@ -17,10 +17,7 @@
 package controllers
 
 import controllers.actions.{AuthAction, FakeAuthAction}
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import akka.util.ByteString
-import config.AppConfig
 import connectors.MessageConnector
 import org.mockito.Matchers.any
 import org.mockito.Mockito.{reset, when}
@@ -29,15 +26,12 @@ import org.scalatestplus.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterEach, FreeSpec, MustMatchers, OptionValues}
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.http.HeaderNames
-import play.api.mvc.{AnyContentAsEmpty, BodyParsers}
+import play.api.mvc.{AnyContentAsEmpty}
 import play.api.test.{FakeHeaders, FakeRequest}
 import play.api.test.Helpers._
 
 import scala.xml.NodeSeq
-import services.XmlValidationService
-import uk.gov.hmrc.auth.core.AuthConnector
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import uk.gov.hmrc.http.{HttpResponse, Upstream5xxResponse}
@@ -68,7 +62,7 @@ class ArrivalsControllerSpec extends FreeSpec with MustMatchers with GuiceOneApp
  "POST /movements/arrivals" - {
    "must return Accepted when successful" in {
      when(mockMessageConnector.post(any())(any(), any()))
-       .thenReturn(Future.successful(HttpResponse(NO_CONTENT)))
+       .thenReturn(Future.successful( HttpResponse(responseStatus = NO_CONTENT, responseJson = None, responseHeaders = Map(LOCATION -> Seq("/arrivals/123")), responseString = None) ))
 
      val request = ctcFakeRequestXML(
        <CC007A>
@@ -109,6 +103,7 @@ class ArrivalsControllerSpec extends FreeSpec with MustMatchers with GuiceOneApp
      val result = route(app, request).value
 
      status(result) mustBe ACCEPTED
+     headers(result) must contain (LOCATION -> "/movements/arrivals/123")
    }
 
    "must return InternalServerError when unsuccessful" in {
@@ -154,6 +149,96 @@ class ArrivalsControllerSpec extends FreeSpec with MustMatchers with GuiceOneApp
      val result = route(app, request).value
 
      status(result) mustBe INTERNAL_SERVER_ERROR
+   }
+
+   "must return InternalServerError when no Location in downstream response header" in {
+     when(mockMessageConnector.post(any())(any(), any()))
+       .thenReturn(Future.successful( HttpResponse(responseStatus = NO_CONTENT, responseJson = None, responseHeaders = Map(), responseString = None) ))
+
+     val request = ctcFakeRequestXML(
+       <CC007A>
+         <SynIdeMES1>UNOC</SynIdeMES1>
+         <SynVerNumMES2>3</SynVerNumMES2>
+         <MesSenMES3>LOCAL-eori</MesSenMES3>
+         <MesRecMES6>NCTS</MesRecMES6>
+         <DatOfPreMES9>20200204</DatOfPreMES9>
+         <TimOfPreMES10>1302</TimOfPreMES10>
+         <IntConRefMES11>WE202002046</IntConRefMES11>
+         <AppRefMES14>NCTS</AppRefMES14>
+         <TesIndMES18>0</TesIndMES18>
+         <MesIdeMES19>1</MesIdeMES19>
+         <MesTypMES20>GB007A</MesTypMES20>
+         <HEAHEA>
+           <DocNumHEA5>99IT9876AB88901209</DocNumHEA5>
+           <CusSubPlaHEA66>EXAMPLE1</CusSubPlaHEA66>
+           <ArrNotPlaHEA60>NW16XE</ArrNotPlaHEA60>
+           <ArrNotPlaHEA60LNG>EN</ArrNotPlaHEA60LNG>
+           <ArrAgrLocOfGooHEA63LNG>EN</ArrAgrLocOfGooHEA63LNG>
+           <SimProFlaHEA132>0</SimProFlaHEA132>
+           <ArrNotDatHEA141>20200204</ArrNotDatHEA141>
+         </HEAHEA>
+         <TRADESTRD>
+           <NamTRD7>EXAMPLE2</NamTRD7>
+           <StrAndNumTRD22>Baker Street</StrAndNumTRD22>
+           <PosCodTRD23>NW16XE</PosCodTRD23>
+           <CitTRD24>London</CitTRD24>
+           <CouTRD25>GB</CouTRD25>
+           <NADLNGRD>EN</NADLNGRD>
+           <TINTRD59>EXAMPLE3</TINTRD59>
+         </TRADESTRD>
+         <CUSOFFPREOFFRES>
+           <RefNumRES1>GB000128</RefNumRES1>
+         </CUSOFFPREOFFRES>
+       </CC007A>
+     )
+     val result = route(app, request).value
+
+     status(result) mustBe INTERNAL_SERVER_ERROR
+   }
+
+   "must escape arrival ID in Location response header" in {
+     when(mockMessageConnector.post(any())(any(), any()))
+       .thenReturn(Future.successful( HttpResponse(responseStatus = NO_CONTENT, responseJson = None, responseHeaders = Map(LOCATION -> Seq("/arrivals/123-@+{`]")), responseString = None) ))
+
+     val request = ctcFakeRequestXML(
+       <CC007A>
+         <SynIdeMES1>UNOC</SynIdeMES1>
+         <SynVerNumMES2>3</SynVerNumMES2>
+         <MesSenMES3>LOCAL-eori</MesSenMES3>
+         <MesRecMES6>NCTS</MesRecMES6>
+         <DatOfPreMES9>20200204</DatOfPreMES9>
+         <TimOfPreMES10>1302</TimOfPreMES10>
+         <IntConRefMES11>WE202002046</IntConRefMES11>
+         <AppRefMES14>NCTS</AppRefMES14>
+         <TesIndMES18>0</TesIndMES18>
+         <MesIdeMES19>1</MesIdeMES19>
+         <MesTypMES20>GB007A</MesTypMES20>
+         <HEAHEA>
+           <DocNumHEA5>99IT9876AB88901209</DocNumHEA5>
+           <CusSubPlaHEA66>EXAMPLE1</CusSubPlaHEA66>
+           <ArrNotPlaHEA60>NW16XE</ArrNotPlaHEA60>
+           <ArrNotPlaHEA60LNG>EN</ArrNotPlaHEA60LNG>
+           <ArrAgrLocOfGooHEA63LNG>EN</ArrAgrLocOfGooHEA63LNG>
+           <SimProFlaHEA132>0</SimProFlaHEA132>
+           <ArrNotDatHEA141>20200204</ArrNotDatHEA141>
+         </HEAHEA>
+         <TRADESTRD>
+           <NamTRD7>EXAMPLE2</NamTRD7>
+           <StrAndNumTRD22>Baker Street</StrAndNumTRD22>
+           <PosCodTRD23>NW16XE</PosCodTRD23>
+           <CitTRD24>London</CitTRD24>
+           <CouTRD25>GB</CouTRD25>
+           <NADLNGRD>EN</NADLNGRD>
+           <TINTRD59>EXAMPLE3</TINTRD59>
+         </TRADESTRD>
+         <CUSOFFPREOFFRES>
+           <RefNumRES1>GB000128</RefNumRES1>
+         </CUSOFFPREOFFRES>
+       </CC007A>
+     )
+     val result = route(app, request).value
+     status(result) mustBe ACCEPTED
+     headers(result) must contain (LOCATION -> "/movements/arrivals/123-%40%2B%7B%60%5D")
    }
 
    "must return UnsupportedMediaType when Content-Type is JSON" in {
