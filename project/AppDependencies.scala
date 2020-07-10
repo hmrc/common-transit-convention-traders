c3141a4768f483733cc4cf69c8964b5b0480c6d1
John Kelly <john.kelly@digital.hmrc.gov.uk>
2020-07-10 13:36:46 +0100

    CTDA-367: Remove unused dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val catsVersion = "2.1.1"
-
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-play-26"   % "1.13.0",
-    "org.typelevel"     %% "cats-core"           % catsVersion
+    "uk.gov.hmrc"       %% "bootstrap-play-26"   % "1.13.0"
   )
 
   val test = Seq(
@@ -21,8 +18,6 @@ object AppDependencies {
     "org.scalatestplus"      %% "mockito-3-2"          % "3.1.2.0",
     "org.scalacheck"         %% "scalacheck"           % "1.14.3",
     "com.github.tomakehurst" % "wiremock-standalone"   % "2.27.1",
-    "org.typelevel"          %% "cats-laws"            % catsVersion,
-    "org.typelevel"          %% "discipline-core"      % "1.0.2",
     "org.typelevel"          %% "discipline-scalatest" % "1.0.1",
     "com.vladsch.flexmark"   % "flexmark-all"          % "0.35.10"
   ).map(_ % "test, it")
