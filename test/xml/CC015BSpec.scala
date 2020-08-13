00f3c24e4e4dedc0a6f2b2d639babf5b2a118b19
Robert Laverick <rob.laverick@digital.hmrc.gov.uk>
2020-08-13 11:46:20 +0100

    forced CC015Bs to be rejected if they contain MesSenMES3 field and added tests

---
+++ test/xml/CC015BSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xml
+
+import data.TestXml
+import models.request.DepartureDeclarationXSD
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import services.{FailedToValidateXml, XmlValidationService}
+
+class CC015BSpec extends AnyFreeSpec with Matchers with ScalaCheckPropertyChecks with TestXml {
+
+  private val xmlValidationService = new XmlValidationService
+
+  "validate" - {
+
+    "must be successful when validating a valid CC015B xml" in {
+        println(CC015B.toString())
+        xmlValidationService.validate(CC015B.toString(), DepartureDeclarationXSD) mustBe a[Right[_, _]]
+    }
+
+    "must fail when validating an invalid CC015B xml" in {
+       xmlValidationService.validate(InvalidCC015B.toString(), DepartureDeclarationXSD) mustBe a[Left[_, _]]
+    }
+
+    "must reject a CC015B containing MesSenMES3" in {
+      xmlValidationService.validate(CC015BwithMesSenMES3.toString(), DepartureDeclarationXSD) mustBe a[Left[_, _]]
+    }
+  }
+}
