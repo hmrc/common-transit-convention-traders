7478fd06cbd199a72f90cf3c536b878f5ff25e4a
GeorgeColdham <george.coldham@opencastsoftware.com>
2020-11-25 13:37:23 +0000

    added test

---
+++ test/audit/AuditServiceSpec.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit
+
+import org.mockito.ArgumentMatchers.{any, eq => eqTo}
+import org.mockito.Mockito.{reset, times, verify}
+import org.scalacheck.Gen
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
+import play.api.test.Helpers.running
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+
+class AuditServiceSpec extends AnyFreeSpec with ScalaCheckPropertyChecks with BeforeAndAfterEach  with MockitoSugar {
+
+  protected def baseApplicationBuilder: GuiceApplicationBuilder =
+    new GuiceApplicationBuilder()
+      .configure(
+        "metrics.jvm" -> false
+      )
+
+  implicit val headerCarrier: HeaderCarrier = HeaderCarrier()
+
+  val mockAuditConnector: AuditConnector = mock[AuditConnector]
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockAuditConnector)
+  }
+
+  "AuditService" - {
+    "must audit notification message event" in {
+
+      val requestXml = <xml>test</xml>
+
+      val auditDetails = Json.toJson(AuditDetails(Json.obj("xml" -> "test")))
+
+      forAll(Gen.oneOf(AuditType.values)) {
+        auditType =>
+          {
+            val application = baseApplicationBuilder
+              .overrides(bind[AuditConnector].toInstance(mockAuditConnector))
+              .build()
+            running(application) {
+              val auditService = application.injector.instanceOf[AuditService]
+              auditService.auditEvent(auditType, requestXml)
+              verify(mockAuditConnector, times(1)).sendExplicitAudit(eqTo(auditType.toString()), eqTo(auditDetails))(any(), any(), any())
+              reset(mockAuditConnector)
+            }
+          }
+      }
+    }
+  }
+}
