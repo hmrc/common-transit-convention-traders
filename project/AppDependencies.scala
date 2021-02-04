7abeb96fa584999bc444da608bf8b673c567c419
GeorgeColdham <georgecoldham@hotmail.co.uk>
2021-02-04 15:21:35 +0000

    updated plugins and dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   private val catsVersion = "2.1.1"
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-play-26"   % "1.13.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-27"   % "3.3.0",
     "org.typelevel"     %% "cats-core"                 % catsVersion,
     "org.json"          % "json"                             % "20200518"
   )
@@ -17,7 +17,7 @@ object AppDependencies {
     "org.typelevel"     %% "cats-core"                 % catsVersion,
     "com.typesafe.play"      %% "play-test"            % current,
     "org.pegdown"            % "pegdown"               % "1.6.0",
-    "org.scalatestplus.play" %% "scalatestplus-play"   % "3.1.3",
+    "org.scalatestplus.play" %% "scalatestplus-play"   % "4.0.3",
     "org.scalatestplus"      %% "mockito-3-2"          % "3.1.2.0",
     "org.scalacheck"         %% "scalacheck"           % "1.14.3",
     "com.github.tomakehurst" % "wiremock-standalone"   % "2.27.1",
