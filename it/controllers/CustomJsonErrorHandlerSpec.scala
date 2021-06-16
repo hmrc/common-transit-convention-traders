db496c56e27934bb4ea43dfcd39e6d877146e017
David Gregory <david.gregory@opencastsoftware.com>
2021-06-16 10:34:13 +0100

    CTDA-1361 Add some more tests for URL encoding, success case

--- it/controllers/CustomJsonErrorHandlerSpec.scala
+++ it/controllers/CustomJsonErrorHandlerSpec.scala
@@ -16,6 +16,9 @@
 
 package controllers
 
+import com.github.tomakehurst.wiremock.client.WireMock._
+import models.domain.Departures
+import models.response.HateoasResponseDepartures
 import org.scalatest.concurrent.IntegrationPatience
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.freespec.AnyFreeSpec
@@ -27,17 +30,59 @@ import play.api.libs.ws.WSClient
 
 import java.util.UUID
 
-class CustomJsonErrorHandlerSpec extends AnyFreeSpec with Matchers with GuiceOneServerPerSuite with utils.WiremockSuite with ScalaFutures with IntegrationPatience {
-  override protected def portConfigKey: Seq[String] = Seq.empty
+class CustomJsonErrorHandlerSpec
+    extends AnyFreeSpec
+    with Matchers
+    with GuiceOneServerPerSuite
+    with utils.WiremockSuite
+    with ScalaFutures
+    with IntegrationPatience {
 
-  private val httpClient = app.injector.instanceOf[WSClient]
-  private val requestId  = UUID.randomUUID().toString
+  override protected def portConfigKey: Seq[String] = Seq(
+    "microservice.services.auth.port",
+    "microservice.services.transits-movements-trader-at-departure.port"
+  )
 
   "CustomJsonErrorHandler" - {
-    "return error message when a bad request is sent" in {
+    "return error message when a bad date format is sent" in {
+      val emptyDepartures = Departures(Seq.empty, 0, 0)
+
+      server.stubFor(
+        post(urlEqualTo("/auth/authorise"))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(
+                """
+                |{
+                |  "authorisedEnrolments": [{
+                |    "key": "HMCE-NCTS-ORG",
+                |    "identifiers": [{
+                |      "key": "VatRegNoTURN",
+                |      "value": "1234567"
+                |    }],
+                |    "state": "Active"
+                |  }]
+                |}
+                """.trim.stripMargin
+              )
+          )
+      )
+
+      server.stubFor(
+        get(urlPathEqualTo("/transits-movements-trader-at-departure/movements/departures"))
+          .willReturn(aResponse().withBody(Json.stringify(Json.toJson(emptyDepartures))).withStatus(OK))
+      )
+
+      val httpClient = app.injector.instanceOf[WSClient]
+      val requestId  = UUID.randomUUID().toString
       val response = httpClient
-        .url(s"http://localhost:$port/movements/departures?updatedSince=15-06-2021T14:55:55.000Z")
-        .withHttpHeaders("channel" -> "api", "X-Request-ID" -> requestId)
+        .url(s"http://localhost:$port/movements/departures?updatedSince=15-06-2021")
+        .withHttpHeaders(
+          "Accept" -> "application/vnd.hmrc.1.0+json",
+          "channel" -> "api",
+          "X-Request-ID" -> requestId
+        )
         .get()
         .futureValue
 
@@ -45,8 +90,106 @@ class CustomJsonErrorHandlerSpec extends AnyFreeSpec with Matchers with GuiceOne
 
       response.json shouldBe Json.obj(
         "statusCode" -> BAD_REQUEST,
-        "message" -> "Cannot parse parameter updatedSince as a valid ISO 8601 timestamp, e.g. 2015-09-08T01:55:28+00:00"
+        "message"    -> "Cannot parse parameter updatedSince as a valid ISO 8601 timestamp, e.g. 2015-09-08T01:55:28+00:00"
       )
+    }
+
+    "return error message when the date is not URL encoded" in {
+      val emptyDepartures = Departures(Seq.empty, 0, 0)
+
+      server.stubFor(
+        post(urlEqualTo("/auth/authorise"))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(
+                """
+                |{
+                |  "authorisedEnrolments": [{
+                |    "key": "HMCE-NCTS-ORG",
+                |    "identifiers": [{
+                |      "key": "VatRegNoTURN",
+                |      "value": "1234567"
+                |    }],
+                |    "state": "Active"
+                |  }]
+                |}
+                """.trim.stripMargin
+              )
+          )
+      )
+
+      server.stubFor(
+        get(urlPathEqualTo("/transits-movements-trader-at-departure/movements/departures"))
+          .willReturn(aResponse().withBody(Json.stringify(Json.toJson(emptyDepartures))).withStatus(OK))
+      )
+
+      val httpClient = app.injector.instanceOf[WSClient]
+      val requestId  = UUID.randomUUID().toString
+      val response = httpClient
+        .url(s"http://localhost:$port/movements/departures?updatedSince=2021-06-15T10:13:05+00:00")
+        .withHttpHeaders(
+          "Accept" -> "application/vnd.hmrc.1.0+json",
+          "channel" -> "api",
+          "X-Request-ID" -> requestId
+        )
+        .get()
+        .futureValue
+
+      response.status shouldBe BAD_REQUEST
+
+      response.json shouldBe Json.obj(
+        "statusCode" -> BAD_REQUEST,
+        "message"    -> "Cannot parse parameter updatedSince as a valid ISO 8601 timestamp, e.g. 2015-09-08T01:55:28+00:00"
+      )
+    }
+
+    "succeed when the date is in the correct format" in {
+      val emptyDepartures = Departures(Seq.empty, 0, 0)
+
+      server.stubFor(
+        post(urlEqualTo("/auth/authorise"))
+          .willReturn(
+            aResponse()
+              .withStatus(OK)
+              .withBody(
+                """
+                |{
+                |  "authorisedEnrolments": [{
+                |    "key": "HMCE-NCTS-ORG",
+                |    "identifiers": [{
+                |      "key": "VatRegNoTURN",
+                |      "value": "1234567"
+                |    }],
+                |    "state": "Active"
+                |  }]
+                |}
+                """.trim.stripMargin
+              )
+          )
+      )
+
+      server.stubFor(
+        get(urlPathEqualTo("/transits-movements-trader-at-departure/movements/departures"))
+          .willReturn(aResponse().withBody(Json.stringify(Json.toJson(emptyDepartures))).withStatus(OK))
+      )
+
+      val httpClient = app.injector.instanceOf[WSClient]
+      val requestId  = UUID.randomUUID().toString
+
+      val response = httpClient
+        .url(s"http://localhost:$port/movements/departures?updatedSince=2021-06-15T10%3A13%3A05%2B00%3A00")
+        .withHttpHeaders(
+          "Accept" -> "application/vnd.hmrc.1.0+json",
+          "channel" -> "api",
+          "X-Request-ID" -> requestId
+        )
+        .get()
+        .futureValue
+
+      response.status shouldBe OK
+
+      response.json shouldBe HateoasResponseDepartures(emptyDepartures)
     }
   }
 }
