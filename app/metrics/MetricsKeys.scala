e6e23c2c28020dad1526d94832d3d301cd624080
GeorgeColdham <georgecoldham@hotmail.co.uk>
2021-06-24 16:43:37 +0100

    Added metrics and keys

--- app/metrics/MetricsKeys.scala
+++ app/metrics/MetricsKeys.scala
@@ -17,45 +17,54 @@
 package metrics
 
 object MetricsKeys {
+
   object ArrivalBackend {
-    val Post = "arrival-backend-post"
-    val Put = "arrival-backend-put"
-    val GetById = "arrival-backend-get-by-id"
-    val GetForEori = "arrival-backend-get-for-eori"
-    val PostMessage = "arrival-backend-post-message"
-    val GetMessageById = "arrival-backend-get-message-by-id"
+    val Post                  = "arrival-backend-post"
+    val Put                   = "arrival-backend-put"
+    val GetById               = "arrival-backend-get-by-id"
+    val GetForEori            = "arrival-backend-get-for-eori"
+    val PostMessage           = "arrival-backend-post-message"
+    val GetMessageById        = "arrival-backend-get-message-by-id"
     val GetMessagesForArrival = "arrival-backend-get-messages-for-arrival"
   }
 
   object DeparturesBackend {
-    val Post = "departures-backend-post"
-    val GetById = "departures-backend-get-by-id"
-    val GetForEori = "departures-backend-get-for-eori"
-    val PostMessage = "departures-backend-post-message"
-    val GetMessageById = "departures-backend-get-message-by-id"
+    val Post                    = "departures-backend-post"
+    val GetById                 = "departures-backend-get-by-id"
+    val GetForEori              = "departures-backend-get-for-eori"
+    val PostMessage             = "departures-backend-post-message"
+    val GetMessageById          = "departures-backend-get-message-by-id"
     val GetMessagesForDeparture = "departures-backend-get-messages-for-departure"
   }
 
   object Endpoints {
     def count(metricKey: String) = s"$metricKey-count"
     // Arrivals
-    val GetArrival = "get-arrival"
-    val GetArrivalsForEori = "get-arrivals-for-eori"
-    val GetArrivalsForEoriCount = count(GetArrivalsForEori)
-    val GetArrivalMessage ="get-arrival-message"
-    val GetArrivalMessages = "get-arrival-messages"
-    val GetArrivalMessagesCount = count(GetArrivalMessages)
-    val SendArrivalMessage = "send-arrival-message"
-    val CreateArrivalNotification = "create-arrival-notification"
+    val GetArrival                  = "get-arrival"
+    val GetArrivalsForEori          = "get-arrivals-for-eori"
+    val GetArrivalsForEoriCount     = count(GetArrivalsForEori)
+    val GetArrivalMessage           = "get-arrival-message"
+    val GetArrivalMessages          = "get-arrival-messages"
+    val GetArrivalMessagesCount     = count(GetArrivalMessages)
+    val SendArrivalMessage          = "send-arrival-message"
+    val CreateArrivalNotification   = "create-arrival-notification"
     val ResubmitArrivalNotification = "resubmit-arrival-notification"
     // Departures
-    val GetDeparture = "get-departure"
-    val GetDeparturesForEori = "get-departures-for-eori"
-    val GetDeparturesForEoriCount = count(GetDeparturesForEori)
-    val GetDepartureMessage = "get-departure-message"
-    val GetDepartureMessages = "get-departure-messages"
-    val GetDepartureMessagesCount = count(GetDepartureMessages)
-    val SendDepartureMessage = "send-departure-message"
+    val GetDeparture               = "get-departure"
+    val GetDeparturesForEori       = "get-departures-for-eori"
+    val GetDeparturesForEoriCount  = count(GetDeparturesForEori)
+    val GetDepartureMessage        = "get-departure-message"
+    val GetDepartureMessages       = "get-departure-messages"
+    val GetDepartureMessagesCount  = count(GetDepartureMessages)
+    val SendDepartureMessage       = "send-departure-message"
     val SubmitDepartureDeclaration = "submit-departure-declaration"
+  }
+
+  object Messages {
+    val MessageSize             = "message-size"
+    val NumberOfGoods           = "number-of-goods"
+    val NumberOfDocuments       = "number-of-documents"
+    val NumberOfSpecialMentions = "number-of-special-mentions"
+    val NumberOfSeals           = "number-of-seals"
   }
 }
---
+++ app/utils/analysis/MessageAnalyser.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.analysis
+
+import scala.xml.NodeSeq
+import java.nio.charset.StandardCharsets
+import com.kenshoo.play.metrics.Metrics
+import metrics.MetricsKeys.Messages._
+import javax.inject.Inject
+import javax.inject.Singleton
+import metrics.HasMetrics
+
+@Singleton
+class MessageAnalyser @Inject() (val metrics: Metrics) extends HasMetrics {
+  lazy val messageSize             = histo(MessageSize)
+  lazy val numberOfGoods           = histo(NumberOfGoods)
+  lazy val numberOfDocuments       = histo(NumberOfDocuments)
+  lazy val numberOfSpecialMentions = histo(NumberOfSpecialMentions)
+  lazy val numberOfSeals           = histo(NumberOfSeals)
+
+  def trackMessageSize(xml: NodeSeq): Unit = {
+    val size = xml.toString.getBytes(StandardCharsets.UTF_8).length
+    messageSize.update(size)
+  }
+
+  def trackNumberOfGoods(xml: NodeSeq): Unit = {
+    val count = (xml \ "GOOITEGDS").length
+    numberOfGoods.update(count)
+  }
+
+  def trackNumberOfDocuments(xml: NodeSeq): Unit = (xml \ "GOOITEGDS")
+    .foreach {
+      node =>
+        val count = (node \ "DOCUMENTNAMETAGTHING").length
+        numberOfDocuments.update(count)
+    }
+
+  def trackNumberOfSpecialMentions(xml: NodeSeq): Unit = (xml \ "GOOITEGDS")
+    .foreach {
+      node =>
+        val count = (node \ "SPEMENMT2").length
+        numberOfSpecialMentions.update(count)
+    }
+
+  def trackNumberOfSeals(xml: NodeSeq): Unit = {
+    val count = (xml \ "SEAIDSID").length
+    numberOfSeals.update(count)
+  }
+}
+
+// TODO put all names in constants file
