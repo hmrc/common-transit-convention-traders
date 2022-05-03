8bce0f1d2d6492fc8ef8c704e279ef7b62e30383
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2022-05-03 18:33:56 +0100

    CTCP-304: Introduce Akka streams

--- test/controllers/stream/VersionedRoutingSpec.scala
+++ test/controllers/stream/VersionedRoutingSpec.scala
@@ -14,9 +14,13 @@
  * limitations under the License.
  */
 
-package controllers
+package controllers.stream
 
+import akka.NotUsed
+import akka.stream.Materializer
+import akka.stream.scaladsl.Source
 import akka.stream.testkit.NoMaterializer
+import akka.util.ByteString
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
@@ -25,43 +29,65 @@ import play.api.http.HeaderNames
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.mvc.Action
 import play.api.mvc.ControllerComponents
-import play.api.test.Helpers.contentAsString
-import play.api.test.Helpers.defaultAwaitTimeout
 import play.api.test.FakeHeaders
 import play.api.test.FakeRequest
+import play.api.test.Helpers.contentAsString
+import play.api.test.Helpers.defaultAwaitTimeout
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
+import java.nio.charset.StandardCharsets
+import scala.collection.immutable
 import scala.concurrent.Future
 import scala.xml.NodeSeq
 
-class VersionSwitchSpec extends AnyFreeSpec
-  with Matchers
-  with GuiceOneAppPerSuite {
+class VersionedRoutingSpec extends AnyFreeSpec with Matchers with GuiceOneAppPerSuite {
 
-  class Harness(cc: ControllerComponents) extends BackendController(cc) with VersionSwitch with Logging {
-    def test: Action[NodeSeq] = versionSwitch(actionOne, actionTwo)
-    def actionOne: Action[NodeSeq] = Action.async(parse.xml) { _ => Future.successful(Ok("One")) }
-    def actionTwo: Action[NodeSeq] = Action.async(parse.xml) { _ => Future.successful(Ok("Two")) }
+  class Harness(cc: ControllerComponents)(implicit val materializer: Materializer)
+      extends BackendController(cc)
+      with VersionedRouting
+      with StreamingParsers
+      with Logging {
+
+    def test: Action[Source[ByteString, _]] = route {
+      case Some("application/vnd.hmrc.2.0+json") => actionTwo
+      case _                                     => actionOne
+    }
+
+    def actionOne: Action[NodeSeq] = Action.async(parse.xml) {
+      _ => Future.successful(Ok("One"))
+    }
+
+    def actionTwo: Action[NodeSeq] = Action.async(parse.xml) {
+      _ => Future.successful(Ok("Two"))
+    }
   }
 
   override lazy val app = GuiceApplicationBuilder()
     .build()
+  implicit lazy val materializer: Materializer = app.materializer
 
-  "VersionSwitch" - {
+  private def generateSource(string: String): Source[ByteString, NotUsed] =
+    Source(ByteString.fromString(string, StandardCharsets.UTF_8).grouped(1024).to[immutable.Iterable])
+
+  "VersionRouting" - {
 
     Seq(None, Some("application/vnd.hmrc.1.0+json"), Some("text/html"), Some("application/vnd.hmrc.1.0+xml"), Some("text/javascript")).foreach {
       acceptHeaderValue =>
-        val acceptHeader = acceptHeaderValue.map(header => Seq(HeaderNames.ACCEPT -> header)).getOrElse(Seq.empty)
-        val departureHeaders =  FakeHeaders(acceptHeader ++ Seq(HeaderNames.CONTENT_TYPE -> "application/xml"))
+        val acceptHeader = acceptHeaderValue
+          .map(
+            header => Seq(HeaderNames.ACCEPT -> header)
+          )
+          .getOrElse(Seq.empty)
+        val departureHeaders = FakeHeaders(acceptHeader ++ Seq(HeaderNames.CONTENT_TYPE -> "application/xml"))
         val withString = acceptHeaderValue
           .getOrElse("nothing")
         s"with accept header set to $withString" - {
 
           "must call correct action" in {
-            val cc = app.injector.instanceOf[ControllerComponents]
+            val cc  = app.injector.instanceOf[ControllerComponents]
             val sut = new Harness(cc)
 
-            val request = FakeRequest("GET","/", departureHeaders, <test>test</test>)
+            val request = FakeRequest("GET", "/", departureHeaders, generateSource("<test>test</test>"))
 
             val result = sut.test()(request)
             contentAsString(result)(defaultAwaitTimeout, NoMaterializer) mustBe "One"
@@ -71,14 +97,14 @@ class VersionSwitchSpec extends AnyFreeSpec
 
     "with accept header set to application/vnd.hmrc.2.0+json (version two)" - {
 
-      val departureHeaders =  FakeHeaders(Seq(HeaderNames.ACCEPT -> "application/vnd.hmrc.2.0+json", HeaderNames.CONTENT_TYPE -> "application/xml"))
+      val departureHeaders = FakeHeaders(Seq(HeaderNames.ACCEPT -> "application/vnd.hmrc.2.0+json", HeaderNames.CONTENT_TYPE -> "application/xml"))
 
       "must call correct action" in {
 
-        val cc = app.injector.instanceOf[ControllerComponents]
+        val cc  = app.injector.instanceOf[ControllerComponents]
         val sut = new Harness(cc)
 
-        val request = FakeRequest("GET","/", departureHeaders, <test>test</test>)
+        val request = FakeRequest("GET", "/", departureHeaders, generateSource("<test>test</test>"))
 
         val result = sut.test()(request)
         contentAsString(result)(defaultAwaitTimeout, NoMaterializer) mustBe "Two"
