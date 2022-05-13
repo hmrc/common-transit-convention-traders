1d7534732a4ad7a01bf4506d236ab90888c79c3a
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2022-05-13 12:38:27 +0100

    CTCP-266: Add integration tests

--- it/v2/connectors/ValidationConnectorSpec.scala
+++ it/v2/connectors/ValidationConnectorSpec.scala
@@ -23,55 +23,91 @@ import com.github.tomakehurst.wiremock.client.WireMock.equalTo
 import com.github.tomakehurst.wiremock.client.WireMock.post
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import config.AppConfig
-import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.IntegrationPatience
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.Application
 import play.api.http.ContentTypes
 import play.api.http.HeaderNames
+import play.api.http.Status.BAD_REQUEST
 import play.api.http.Status.OK
-import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.JsArray
+import play.api.libs.json.JsValue
 import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.mvc.Http.MimeTypes
+import uk.gov.hmrc.http
 import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.UpstreamErrorResponse
 import utils.TestMetrics
+import v2.models.errors.BaseError
 import v2.models.request.MessageType
 import v2.models.responses.ValidationResponse
 
 import java.nio.charset.StandardCharsets
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
 
-class ValidationConnectorSpec extends AnyFreeSpec with Matchers with GuiceOneAppPerSuite with utils.WiremockSuite with BeforeAndAfterEach {
+class ValidationConnectorSpec extends AnyFreeSpec
+  with Matchers
+  with GuiceOneAppPerSuite
+  with utils.WiremockSuite
+  with ScalaFutures
+  with IntegrationPatience {
 
-  override lazy val app: Application = GuiceApplicationBuilder().build()
-  val wsclient: WSClient             = app.injector.instanceOf[WSClient]
-  val appConfig: AppConfig           = app.injector.instanceOf[AppConfig]
+  lazy val wsclient: WSClient             = app.injector.instanceOf[WSClient]
+  lazy val appConfig: AppConfig           = app.injector.instanceOf[AppConfig]
 
-  val validationConnector: ValidationConnectorImpl = new ValidationConnectorImpl(wsclient, appConfig, new TestMetrics())
-
-  implicit val ec: ExecutionContext = app.materializer.executionContext
+  lazy val validationConnector: ValidationConnectorImpl = new ValidationConnectorImpl(wsclient, appConfig, new TestMetrics())
+  implicit lazy val ec: ExecutionContext = app.materializer.executionContext
 
   "POST /message/:messageType/validate" - {
 
     "On successful validation of schema valid XML, must return OK and empty validation error" in {
+
       server.stubFor(
         post(
           urlEqualTo("/transit-movements-validator/message/IE015C/validate") // /transit-movements-validator/message/IE015C/validate
         )
-         // .withHeader(HeaderNames.ACCEPT, equalTo(ContentTypes.JSON))
-         // .withHeader(HeaderNames.CONTENT_TYPE, equalTo(MimeTypes.XML))
+        .withHeader(HeaderNames.CONTENT_TYPE, equalTo(MimeTypes.XML))
+        .willReturn(
+          aResponse()
+            .withStatus(OK)
+            .withBody(
+              Json.stringify(
+                Json.toJson(
+                  ValidationResponse(Seq())
+                )
+              )
+            )
+          )
+      )
+
+      implicit val hc: HeaderCarrier = HeaderCarrier(extraHeaders = Seq(HeaderNames.ACCEPT -> ContentTypes.JSON))
+
+      val source = Source.single(ByteString("<test></test>", StandardCharsets.UTF_8)) // TODO: IE015C
+
+      val result = validationConnector.validate(MessageType.DepartureDeclaration, source)
+      Await.result(result, 5.seconds) mustBe Json.obj("validationErrors" -> JsArray())
+    }
+
+    "On successful validation of schema invalid XML, must return OK and a validation error" in {
+
+      server.stubFor(
+        post(
+          urlEqualTo("/transit-movements-validator/message/IE015C/validate") // /transit-movements-validator/message/IE015C/validate
+        )
+          .withHeader(HeaderNames.CONTENT_TYPE, equalTo(MimeTypes.XML))
           .willReturn(
             aResponse()
               .withStatus(OK)
               .withBody(
                 Json.stringify(
                   Json.toJson(
-                    ValidationResponse(Seq())
+                    ValidationResponse(Seq("nope"))
                   )
                 )
               )
@@ -80,10 +116,42 @@ class ValidationConnectorSpec extends AnyFreeSpec with Matchers with GuiceOneApp
 
       implicit val hc: HeaderCarrier = HeaderCarrier(extraHeaders = Seq(HeaderNames.ACCEPT -> ContentTypes.JSON))
 
-      val source = Source.single(ByteString("<test></test>", StandardCharsets.UTF_8))
+      val source = Source.single(ByteString("<test></test>", StandardCharsets.UTF_8)) // TODO: IE015C
 
       val result = validationConnector.validate(MessageType.DepartureDeclaration, source)
-      Await.result(result, 5.seconds) mustBe Json.obj("validationErrors" -> JsArray())
+      Await.result(result, 5.seconds) mustBe Json.obj("validationErrors" -> Seq("nope")) // TODO
+    }
+
+    "On invalid XML, must return BAD_REQUEST and an appropriate error message" in {
+
+      server.stubFor(
+        post(
+          urlEqualTo("/transit-movements-validator/message/IE015C/validate") // /transit-movements-validator/message/IE015C/validate
+        )
+          .withHeader(HeaderNames.CONTENT_TYPE, equalTo(MimeTypes.XML))
+          .willReturn(
+            aResponse()
+              .withStatus(BAD_REQUEST)
+              .withBody(
+                Json.stringify(
+                  Json.toJson(
+                    BaseError.badRequestError("Invalid XML") // TODO
+                  )
+                )
+              )
+          )
+      )
+
+      implicit val hc: HeaderCarrier = HeaderCarrier(extraHeaders = Seq(HeaderNames.ACCEPT -> ContentTypes.JSON))
+
+      val source = Source.single(ByteString("<test></test>", StandardCharsets.UTF_8)) // TODO: IE015C
+
+      val result: Future[JsValue] = Await.ready(validationConnector.validate(MessageType.DepartureDeclaration, source), 5.seconds)
+
+      val thr = result.eitherValue.get.left.get
+      thr mustBe a [http.UpstreamErrorResponse]
+      thr.asInstanceOf[UpstreamErrorResponse].statusCode mustBe BAD_REQUEST
+      Json.parse(thr.asInstanceOf[UpstreamErrorResponse].message) mustBe Json.obj("code" -> "BAD_REQUEST", "message" -> "Invalid XML")
     }
 
   }
