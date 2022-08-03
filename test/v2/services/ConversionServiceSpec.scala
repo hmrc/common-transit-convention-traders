7b7597c78b7306bb2da4a563bc4e1c7d36d432aa
Derek Palmer <35488891+derekspalmer@users.noreply.github.com>
2022-08-03 15:13:17 +0100

    CTCP-737: wire-up the  Converter

---
+++ test/v2/services/ConversionServiceSpec.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.services
+
+import akka.NotUsed
+import akka.stream.Materializer
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.http.Status.INTERNAL_SERVER_ERROR
+import uk.gov.hmrc.http.{HeaderCarrier, UpstreamErrorResponse}
+import v2.base.TestActorSystem
+import v2.connectors.ConversionConnector
+import v2.models.request.MessageType
+
+import java.nio.charset.StandardCharsets
+import scala.concurrent._
+
+class ConversionServiceSpec extends AnyFreeSpec with Matchers with MockitoSugar with ScalaFutures with TestActorSystem {
+
+  "On converting a message" - {
+    "a successful conversion, should return a Right" in new Setup {
+      val result = sut.convert(MessageType.DepartureDeclaration, jsonPayload)
+      whenReady(result.value) {
+        _.isRight
+      }
+      verify(mockConnector, times(1)).post(any(), any())(any(), any(), any())
+    }
+
+    "a failed conversion, should return a Left" in new Setup {
+      when(mockConnector.post(any[MessageType], any[Source[ByteString, _]]())(any[HeaderCarrier], any[ExecutionContext], any[Materializer]))
+        .thenReturn(Future.failed(upstreamErrorResponse))
+      val result = sut.convert(MessageType.DepartureDeclaration, jsonPayload)
+      whenReady(result.value) {
+        _.isLeft
+      }
+      verify(mockConnector, times(1)).post(any(), any())(any(), any(), any())
+    }
+  }
+
+  trait Setup {
+    implicit val ec: ExecutionContext = materializer.executionContext
+    implicit val hc: HeaderCarrier    = HeaderCarrier()
+
+    val upstreamErrorResponse: Throwable          = UpstreamErrorResponse("Internal service error", INTERNAL_SERVER_ERROR)
+    val mockResponse: Source[ByteString, NotUsed] = Source.single(ByteString(<schemaValid></schemaValid>.mkString, StandardCharsets.UTF_8))
+    val jsonPayload: Source[ByteString, NotUsed]  = Source.single(ByteString("{}", StandardCharsets.UTF_8))
+
+    val mockConnector = mock[ConversionConnector]
+    when(mockConnector.post(any[MessageType], any[Source[ByteString, _]]())(any[HeaderCarrier], any[ExecutionContext], any[Materializer]))
+      .thenReturn(Future(mockResponse))
+
+    val sut = new ConversionServiceImpl(mockConnector)
+  }
+
+}
