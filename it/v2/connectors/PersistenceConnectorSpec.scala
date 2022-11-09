f28dabea158b1b8b27b27f810b2ed73a303df032
Sandeep Kumar Soni <112713042+sonisn1988@users.noreply.github.com>
2022-11-09 16:56:03 +0000

    review comments changes

--- it/v2/connectors/PersistenceConnectorSpec.scala
+++ it/v2/connectors/PersistenceConnectorSpec.scala
@@ -958,7 +958,7 @@ class PersistenceConnectorSpec
     "on successful return of arrival message IDs when filtering by received date is applied, return a success" in forAll(
       arbitrary[EORINumber],
       arbitrary[MovementId],
-      arbitrary[OffsetDateTime].sample.get,
+      OffsetDateTime.now(ZoneOffset.UTC),
       Gen.nonEmptyListOf(arbitrary[MessageSummary])
     ) {
       (eori, arrivalId, time, messageResponse) =>
