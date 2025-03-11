bd337c98830d795de7eec72a0952f8003be2d8ff
damien.butterworth <damienbutterworth@hotmail.co.uk>
2025-03-11 12:03:00 +0000

    CTCP-6064: Upgrade bootstrap and sbt-plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val catsVersion          = "2.13.0"
-  private val bootstrapPlayVersion = "9.10.0"
+  private val bootstrapPlayVersion = "9.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"      %% "bootstrap-backend-play-30"       % bootstrapPlayVersion,
